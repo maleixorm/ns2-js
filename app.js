80e8d315eb5425c971a699763ff10a6a48b2ce9c
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-10-20 15:22:15 -0300

    Inserindo informações com o JS

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+let paragrafo = document.querySelector('p');
+
+titulo.innerHTML = `Jogo do Número Secreto`;
+paragrafo.innerHTML = `Escolha um número entre 1 e 10.`;
